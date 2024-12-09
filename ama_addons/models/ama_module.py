4f00610c4384ee80a9b44d9d941adef9760416d4
Fajar Zulmi Sopian <fajar_zulmi@pmr.net>
2024-12-09 11:19:42 +0700

    Update

--- ama_addons/models/ama_module.py
+++ ama_addons/models/ama_module.py
@@ -1,5 +1,25 @@
 from odoo import api, _, fields, models
 
+class StockMoveInherit(models.Model):
+    _inherit = 'stock.move'
+
+    ama_onhand_quantity = fields.Float(string="On Hand Total", compute="_compute_onhand_head", digits=(16,3))
+    ama_reserve_quantity = fields.Float(string="Reserved Total", compute="_compute_reserve_head", digits=(16,3))
+
+    @api.depends('product_id')
+    def _compute_onhand_head(self):
+        for rec in self :
+            product = rec.product_id
+            var = rec.move_line_ids.filtered(lambda e:e.product_id.id == product.id).mapped('ama_onhand_quantity')
+            rec.ama_onhand_quantity = sum(var)
+    
+    @api.depends('product_id')
+    def _compute_reserve_head(self):
+        for rec in self :
+            product = rec.product_id
+            var = rec.move_line_ids.filtered(lambda e:e.product_id.id == product.id).mapped('ama_reserve_quantity')
+            rec.ama_reserve_quantity = sum(var)
+
 class StockMoveLineInherit(models.Model):
     _inherit = 'stock.move.line'
 
